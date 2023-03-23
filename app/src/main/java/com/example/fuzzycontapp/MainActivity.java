071dcff3e09de4502ecb7ec8158a29171db2d135
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-03-23 23:59:24 +0300

    sens

--- app/src/main/java/com/example/fuzzycontapp/MainActivity.java
+++ app/src/main/java/com/example/fuzzycontapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.fuzzycontapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(listener);
     }
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Sensor.class);
+            startActivity(intent);
+        }
+    };
 }
