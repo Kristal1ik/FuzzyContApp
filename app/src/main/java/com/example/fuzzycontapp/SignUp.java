35c42778871083454fcc472b2bea4d4aa9372673
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-04-05 22:46:23 +0300

    fragment sign up

---
+++ app/src/main/java/com/example/fuzzycontapp/SignUp.java
@@ -0,0 +1,18 @@
+package com.example.fuzzycontapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SignUp extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+}
