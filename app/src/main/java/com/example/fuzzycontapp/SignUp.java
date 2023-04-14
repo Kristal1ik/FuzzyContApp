2232f6beb3948763547d65714dca22df5e9c7384
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-04-15 02:35:10 +0300

    sign up

--- app/src/main/java/com/example/fuzzycontapp/SignUp.java
+++ app/src/main/java/com/example/fuzzycontapp/SignUp.java
@@ -2,17 +2,43 @@ package com.example.fuzzycontapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fuzzycontapp.databinding.FragmentSignupBinding;
+
 
 public class SignUp extends Fragment {
+    FragmentSignupBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        binding = FragmentSignupBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = binding.usernameSignup.getText().toString();
+                String password = binding.passwordSignup.getText().toString();
+                String confirm = binding.againPasswordSignup.getText().toString();
+                binding.usernameSignup.setText(""); binding.passwordSignup.setText(""); binding.againPasswordSignup.setText("");
+            }
+        });
     }
 }
