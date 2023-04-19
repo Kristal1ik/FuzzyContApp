11e23ab369e61f9b22df56eb5fea12c4427d3711
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-04-19 10:43:17 +0300

    menu

--- app/src/main/java/com/example/fuzzycontapp/MenuActivity.java
+++ app/src/main/java/com/example/fuzzycontapp/MenuActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Sensor extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sensor);
+        setContentView(R.layout.activity_menu);
     }
 }
