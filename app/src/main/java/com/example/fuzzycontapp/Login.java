fab1aa5e0ed59a29e089d0de8ad3594a01235454
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-04-21 17:29:22 +0300

    sock

---
+++ app/src/main/java/com/example/fuzzycontapp/Login.java
@@ -0,0 +1,56 @@
+package com.example.fuzzycontapp;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fuzzycontapp.databinding.FragmentLoginBinding;
+
+
+public class Login extends Fragment {
+    FragmentLoginBinding binding;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String login = binding.usernameLogin.getText().toString();
+                String password = binding.passwordLogin.getText().toString();
+                binding.usernameLogin.setText(""); binding.passwordLogin.setText("");
+
+                Intent intent = new Intent(v.getContext(), MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
--- app/src/main/java/com/example/fuzzycontapp/MainActivity.java
+++ app/src/main/java/com/example/fuzzycontapp/MainActivity.java
@@ -1,26 +1,87 @@
 package com.example.fuzzycontapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
     Button button;
-    @Override
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    MainAdapter mainAdapter;
+    MyThread myThread;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         button = findViewById(R.id.button);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        viewPager2 = (ViewPager2) findViewById(R.id.view_pager);
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+
         button.setOnClickListener(listener);
+        myThread = new MyThread();
+        new Thread(myThread).start();
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mainAdapter = new MainAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(mainAdapter);
+
     }
+
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, Sensor.class);
+            Intent intent = new Intent(MainActivity.this, Accelerometer.class);
             startActivity(intent);
         }
     };
+    static class MyThread implements Runnable{
+        private volatile String msg = "";
+        public static Socket sock;
+        DataOutputStream output;
+
+        @Override
+        public void run() {
+            try{
+                sock = new Socket("192.168.0.17", 22345);
+                output = new DataOutputStream(sock.getOutputStream());
+                output.writeUTF(msg);
+                output.flush();
+                output.close();
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
