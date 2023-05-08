ee5b139ac10ad64fdb7a7a83f8d13dc6b3c35e0c
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-08 10:14:44 +0300

    category_model

---
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/CategoryRow.java
@@ -0,0 +1,27 @@
+package com.example.fuzzycontapp.Indiv;
+
+public class CategoryRow {
+    public String  name;
+    public int img;
+
+    public CategoryRow(String name, int img) {
+        this.name = name;
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
