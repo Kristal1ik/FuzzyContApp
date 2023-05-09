65d1854d3f5a5efa9e29e67a49ba3eb7b537ac1d
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-09 23:53:57 +0300

    global

---
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/Global.java
@@ -0,0 +1,12 @@
+package com.example.fuzzycontapp.Indiv;
+
+
+import android.graphics.Bitmap;
+
+public class Global {
+    public static String USERNAME = "";
+    public static String EMAIL = "";
+    public static Bitmap IMG;
+//    public static Bitmap IMG_BITMAP = ;
+
+}
