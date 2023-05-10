150a41d051e40cfa4082d7959946506a72f4d912
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-10 04:42:56 +0300

    sw

---
+++ app/src/main/java/com/example/fuzzycontapp/Fragments/Theory.java
@@ -0,0 +1,46 @@
+package com.example.fuzzycontapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fuzzycontapp.Adapters.TheoryAdapter;
+import com.example.fuzzycontapp.Indiv.TheoryRow;
+import com.example.fuzzycontapp.R;
+import com.example.fuzzycontapp.databinding.FragmentHomeBinding;
+import com.example.fuzzycontapp.databinding.FragmentTheoryBinding;
+
+import java.util.ArrayList;
+
+public class Theory extends Fragment {
+    FragmentTheoryBinding binding;
+    ArrayList<TheoryRow> dataholder;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentTheoryBinding.inflate(inflater, container, false);
+        dataholder = new ArrayList<>();
+        dataholder.add(new TheoryRow("Теория нечетких множеств. Алгоритм Мамдани", R.drawable.at));
+        binding.rview.setAdapter(new TheoryAdapter(dataholder));
+
+        binding.rview.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+        return binding.getRoot();
+    }
+
+
+}
