9b700ebcc9e75e5848585a38c946f51219619f9f
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-10 04:42:42 +0300

    sw

---
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/TheoryRow.java
@@ -0,0 +1,28 @@
+package com.example.fuzzycontapp.Indiv;
+
+public class TheoryRow {
+    public String  name;
+    public int img;
+
+    public TheoryRow(String name, int img) {
+        this.name = name;
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
+
