21f1073e92665c4b07db0a18e31d38c05a1f4edc
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-14 20:45:58 +0300

    optim constants

--- app/src/main/java/com/example/fuzzycontapp/Indiv/Const.java
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/Const.java
@@ -1,8 +1,25 @@
 package com.example.fuzzycontapp.Indiv;
 
 public class Const {
-    public static int r = 70;
+    public static int radius = 70;
     public static int strWidth = 20;
-    public static int silWidth = 50;
+    public static int silWidth = 80;
+    public static int borderWidth = 5;
+    public static final double g = 9.8;
+    public static final double m = 0.5;
+    public static final double maxis = 0.01;
+    public static final double r = 0.002;
+    public static final double R = 0.03;
+    public static final double dt = 0.01;
+    public static final double T = 60;
+    public static final double x0 = 0.05;
+    public static final double v0 = 0;
+    public static final double l = 0.5;
+    public static final double x1 = 0.2;
+    public static final double x2 = 0.3;
+    public static final double k = 0.1;
+    public static final double e = 0.000001;
+    public static final int width = 1080;
+    public static final int height = 2131;
 
 }
