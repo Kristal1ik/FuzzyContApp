23c2a8b0ddf7bf4edf4ad8902ec9629ecb5199d9
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-14 20:45:35 +0300

    accelerometer fragment

---
+++ app/src/main/java/com/example/fuzzycontapp/Fragments/AccelerometerFragment.java
@@ -0,0 +1,29 @@
+package com.example.fuzzycontapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.example.fuzzycontapp.R;
+
+public class AccelerometerFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_accelerometer, container, false);
+//        FrameLayout frameLayout = (FrameLayout) rootView.findViewById(R.id.maths_fragment);
+//        frameLayout.addView(new );
+        return rootView;
+    }
+}
