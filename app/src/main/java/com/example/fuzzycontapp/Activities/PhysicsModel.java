5b4ebfc75a831e8c23e9e3c0c38c72b38dc4ef87
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-14 20:49:10 +0300

    phys

--- app/src/main/java/com/example/fuzzycontapp/Activities/PhysicsModel.java
+++ app/src/main/java/com/example/fuzzycontapp/Activities/PhysicsModel.java
@@ -1,10 +1,14 @@
 package com.example.fuzzycontapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.fuzzycontapp.R;
 import com.example.fuzzycontapp.databinding.ActivityMathsModelBinding;
 import com.example.fuzzycontapp.databinding.ActivityPhysicsModelBinding;
 
@@ -16,5 +20,11 @@ public class PhysicsModel extends AppCompatActivity {
         binding = ActivityPhysicsModelBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+    }
+    protected void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_maths, fragment);
+        fragmentTransaction.commit();
     }
 }
