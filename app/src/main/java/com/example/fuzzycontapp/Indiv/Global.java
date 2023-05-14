7e0dc1933a2b250c48e94e69545cd99b0d1ef81a
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-14 20:47:20 +0300

    to read

--- app/src/main/java/com/example/fuzzycontapp/Indiv/Global.java
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/Global.java
@@ -7,6 +7,7 @@ public class Global {
     public static String USERNAME = "";
     public static String EMAIL = "";
     public static Bitmap IMG;
+    public static boolean TO_READ = false;
 //    public static Bitmap IMG_BITMAP = ;
 
 }
