241ee5e966fb3c8d9d065aa88809f9103b2cd670
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-14 20:49:21 +0300

    base

---
+++ app/src/main/java/com/example/fuzzycontapp/FuzzyLogic/Rules.java
@@ -0,0 +1,24 @@
+package com.example.fuzzycontapp.FuzzyLogic;
+
+public class Rules {
+    static Trapezoid x1 = new Trapezoid(new double[]{0.09, 0.14, 0.156, 0.175, 1.0});
+    static Trapezoid v1 = new Trapezoid(new double[]{0.280, 0.351, 0.466, 0.616, 1.0});
+
+    public static double[] w1 = new double[] {6.00, 9.550, 13.550, 16.350, 1.0};
+
+    static Trapezoid x2 = new Trapezoid(new double[] {0.270, 0.320, 0.336, 0.355, 1.0});
+    static Trapezoid v2 = new Trapezoid(new double[] {-0.09, -0.019, 0.096, 0.246, 1.0});
+    public static double[] w2 = new double[] {14.000, 17.550, 21.550, 24.350, 1.0};
+
+    static Trapezoid x3 = new Trapezoid(new double[] {0.010, 0.060, 0.076, 0.095, 1.0});
+    static Trapezoid v3 = new Trapezoid(new double[] {0.280, 0.351, 0.466, 0.616, 1.0});
+    public static double[] w3 = new double[] {6.000, 9.550, 13.550, 16.350, 1.0};
+
+    static Trapezoid x4 = new Trapezoid(new double[] {0.480, 0.530, 0.546, 0.565, 1.0});
+    static Trapezoid v4 = new Trapezoid(new double[] {0.730, 0.801, 0.916, 1.066, 1.0});
+    public static double[] w4 = new double[] {20.000, 23.550, 27.550, 30.350, 1.0};
+
+    static Trapezoid x5 = new Trapezoid(new double[] {0.270, 0.320, 0.336, 0.355, 1.0});
+    static Trapezoid v5 = new Trapezoid(new double[] {-0.090, -0.019, 0.096, 0.246, 1.0});
+    public static double[] w5 = new double[] {20.000, 23.550, 27.550, 30.3500, 1.0};
+}
