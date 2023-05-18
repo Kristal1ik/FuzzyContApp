5c5d78746942846a4d77ee4bbb7374796d069b37
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-19 00:14:42 +0300

    constants for accelerometer

--- app/src/main/java/com/example/fuzzycontapp/Indiv/Const.java
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/Const.java
@@ -1,12 +1,11 @@
 package com.example.fuzzycontapp.Indiv;
 
 public class Const {
-    public static int radius = 70;
-    public static int strWidth = 20;
-    public static int silWidth = 80;
-    public static int borderWidth = 5;
+    public static final int radius = 70;
+    public static final int strWidth = 20;
+    public static final int silWidth = 80;
+    public static final int borderWidth = 5;
     public static final double g = 9.8;
-    public static final double m = 0.5;
     public static final double maxis = 0.01;
     public static final double r = 0.002;
     public static final double R = 0.03;
@@ -15,11 +14,21 @@ public class Const {
     public static final double x0 = 0.05;
     public static final double v0 = 0;
     public static final double l = 0.465;
-    public static final double x1 = 0.2;
-    public static final double x2 = 0.3;
     public static final double k = 0.1;
     public static final double e = 0.000001;
     public static final int width = 1080;
     public static final int height = 2131;
+    public static final double sensitivity = 20;
+    public static  double sensitivity_current = 20;
+    public static final double m = 0.5;
+    public static double m_current = 0.5;
+    public static final double x1 = 0.5;
+    public static final double x2 = 0.5;
+    public static double x1_current = 0.15;
+    public static double x2_current = 0.35;
+    public static int position_mass = 5;
+    public static int position_sens = 3;
+    public static int position_top = 3;
+    public static int position_bottom = 7;
 
 }
