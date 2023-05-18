3125a02ef5b46d3a9acdf1d7a177ab8de5f8d48d
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-18 22:29:52 +0300

    maths

--- app/src/main/java/com/example/fuzzycontapp/Activities/MathsModel.java
+++ app/src/main/java/com/example/fuzzycontapp/Activities/MathsModel.java
@@ -7,10 +7,17 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.LinearLayout;
 
@@ -18,14 +25,18 @@ import com.example.fuzzycontapp.Fragments.MathsFragment;
 
 import com.example.fuzzycontapp.R;
 import com.example.fuzzycontapp.databinding.ActivityMathsModelBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MathsModel extends AppCompatActivity {
     ActivityMathsModelBinding binding;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMathsModelBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.blue));
         setFragment(new MathsFragment());
         setContentView(view);
 
@@ -36,6 +47,19 @@ public class MathsModel extends AppCompatActivity {
             }
         });
 
+        binding.settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MathsModel.this, R.style.BottomSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dialog, (LinearLayout)findViewById(R.id.container));
+
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+
+
+            }
+        });
+
     }
     protected void setFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -43,4 +67,6 @@ public class MathsModel extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout_maths, fragment);
         fragmentTransaction.commit();
     }
+
+
 }
