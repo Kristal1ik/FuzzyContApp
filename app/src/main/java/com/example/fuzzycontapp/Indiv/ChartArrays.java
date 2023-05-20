0d153470e938c43afc3b3f3d31c95b0fa1c958b9
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-21 00:22:44 +0300

    arrays for charts

---
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/ChartArrays.java
@@ -0,0 +1,10 @@
+package com.example.fuzzycontapp.Indiv;
+
+import java.util.ArrayList;
+
+public class ChartArrays {
+    public static ArrayList<Float> XXX = new ArrayList<>();
+    public static ArrayList<Float> VVV = new ArrayList<>();
+    public static ArrayList<Float> WWW = new ArrayList<>();
+    public static ArrayList<Float> TIME = new ArrayList<>();
+}
