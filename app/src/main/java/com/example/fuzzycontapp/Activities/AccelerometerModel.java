83b5e737176f2182e43e3f2bfa93b0d69c9ed599
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-21 00:20:11 +0300

    accelerometer's seekbars

--- app/src/main/java/com/example/fuzzycontapp/Activities/AccelerometerModel.java
+++ app/src/main/java/com/example/fuzzycontapp/Activities/AccelerometerModel.java
@@ -13,8 +13,11 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,8 +27,10 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.fuzzycontapp.Fragments.AccelerometerFragment;
+import com.example.fuzzycontapp.Indiv.Const;
 import com.example.fuzzycontapp.R;
 import com.example.fuzzycontapp.databinding.ActivitySensorBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class AccelerometerModel extends AppCompatActivity {
     ActivitySensorBinding binding;
@@ -58,13 +63,108 @@ public class AccelerometerModel extends AppCompatActivity {
         binding.settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Dialog dialog = new Dialog(context);
-//                dialog.setContentView(R.layout.dialog_fragment);
-//                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
-//                        ViewGroup.LayoutParams.WRAP_CONTENT);
-//                dialog.getWindow().setGravity(Gravity.BOTTOM);
-//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//                dialog.show();
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(AccelerometerModel.this, R.style.BottomSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dialog, (LinearLayout)findViewById(R.id.container));
+                SeekBar seekBar_sens = bottomSheetView.findViewById(R.id.seekbar_sensitivity);
+                SeekBar seekBar_mass = bottomSheetView.findViewById(R.id.seekbar_mass);
+                SeekBar seekBar_bottom = bottomSheetView.findViewById(R.id.seekbar_bottom);
+                SeekBar seekBar_top = bottomSheetView.findViewById(R.id.seekbar_top);
+
+                TextView sens_pr = bottomSheetView.findViewById(R.id.sensitivity_pr);
+                TextView mass_pr = bottomSheetView.findViewById(R.id.mass_pr);
+                TextView bottom_pr = bottomSheetView.findViewById(R.id.bottom);
+                TextView top_pr = bottomSheetView.findViewById(R.id.top);
+
+                seekBar_mass.setProgress(Const.position_mass);
+                mass_pr.setText(Const.m_current + "kg");
+
+                seekBar_sens.setProgress(Const.position_sens);
+                sens_pr.setText(Const.position_sens * 20 + "%");
+
+                seekBar_bottom.setProgress(Const.position_bottom);
+                bottom_pr.setText(Const.x2_current + "m");
+
+                seekBar_top.setProgress(Const.position_top);
+                top_pr.setText(Const.x1_current + "m");
+                seekBar_sens.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+                {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        Const.sensitivity_current = (Const.sensitivity * (progress * 20)) / 100;
+                        sens_pr.setText(progress * 20 + " %");
+                        Const.position_sens = progress;
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+
+                seekBar_mass.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        Const.m_current = (Const.m * (progress * 0.1)) / 0.5;
+                        Const.position_mass = progress;
+                        mass_pr.setText(Const.m_current + " kg");
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+                seekBar_bottom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        Const.x2_current = (Const.x2 * (progress * 5)) / 50;
+                        bottom_pr.setText(Const.x2_current + "m");
+                        Const.position_bottom = progress;
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+                seekBar_top.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        Const.x1_current = (Const.x1 * (progress * 5)) / 50;
+                        top_pr.setText(Const.x1_current + "m");
+                        Const.position_top = progress;
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+
+
+
             }
         });
     }
