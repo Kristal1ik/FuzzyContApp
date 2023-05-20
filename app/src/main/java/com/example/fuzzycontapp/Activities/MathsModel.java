ea168159ef87076343fc39f46d089b7150c13384
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-21 00:39:42 +0300

    seekbars

--- app/src/main/java/com/example/fuzzycontapp/Activities/MathsModel.java
+++ app/src/main/java/com/example/fuzzycontapp/Activities/MathsModel.java
@@ -20,12 +20,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 import com.example.fuzzycontapp.Fragments.MathsFragment;
 
+import com.example.fuzzycontapp.Indiv.ChartArrays;
+import com.example.fuzzycontapp.Indiv.Const;
 import com.example.fuzzycontapp.R;
 import com.example.fuzzycontapp.databinding.ActivityMathsModelBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
 
 public class MathsModel extends AppCompatActivity {
     ActivityMathsModelBinding binding;
@@ -46,15 +51,43 @@ public class MathsModel extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        binding.chart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MathsModel.this, ShowCharts.class);
+                System.out.println(ChartArrays.XXX);
+                System.out.println(ChartArrays.VVV);
+                System.out.println(ChartArrays.WWW);
+                System.out.println(ChartArrays.TIME);
 
+                startActivity(intent);
+            }
+        });
         binding.settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MathsModel.this, R.style.BottomSheetDialogTheme);
                 View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dialog, (LinearLayout)findViewById(R.id.container));
+                SeekBar seekBar_sens = bottomSheetView.findViewById(R.id.seekbar_sensitivity);
+                seekBar_sens.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        System.out.println(progress);
+                    }
 
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
                 bottomSheetDialog.setContentView(bottomSheetView);
                 bottomSheetDialog.show();
+
 
 
             }
