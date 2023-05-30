15bec512a01a444eab8b7118c0cf0c3a2b76fcc9
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-31 00:51:43 +0300

    globals

--- app/src/main/java/com/example/fuzzycontapp/Indiv/Global.java
+++ app/src/main/java/com/example/fuzzycontapp/Indiv/Global.java
@@ -2,13 +2,17 @@ package com.example.fuzzycontapp.Indiv;
 
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 public class Global {
     public static String USERNAME = "";
     public static String EMAIL = "";
-    public static Bitmap IMG;
+    public static Bitmap IMG = null;
     public static boolean TO_READ = false;
     public static int THEORY_NUMBER;
     public static String ADDRESS;
     public static int GLOBAL_RULES = 0;
+    public static int SAVED_RULES = 0;
+    public static int MY_RULES = 0;
+    public static boolean IMG_BOOL = true;
 }
