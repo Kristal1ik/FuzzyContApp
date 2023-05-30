e77a9f48274659167a0431b9530ac1067339b54a
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-05-31 00:52:23 +0300

    basin

---
+++ app/src/main/java/com/example/fuzzycontapp/Fragments/GeneticAlgorithm.java
@@ -0,0 +1,27 @@
+package com.example.fuzzycontapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fuzzycontapp.R;
+
+
+public class GeneticAlgorithm extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_genetic_algorithm, container, false);
+    }
+}
