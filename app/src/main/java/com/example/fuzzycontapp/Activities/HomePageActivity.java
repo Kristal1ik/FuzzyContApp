8a11031adeb10ac2f98da2019da488e7243dd852
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-06-01 04:18:50 +0300

    support

--- app/src/main/java/com/example/fuzzycontapp/Activities/HomePageActivity.java
+++ app/src/main/java/com/example/fuzzycontapp/Activities/HomePageActivity.java
@@ -13,7 +13,12 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 import com.example.fuzzycontapp.Fragments.DifferentRules;
@@ -25,6 +30,7 @@ import com.example.fuzzycontapp.Fragments.Theory;
 import com.example.fuzzycontapp.Indiv.Global;
 import com.example.fuzzycontapp.R;
 import com.example.fuzzycontapp.databinding.ActivityHomePageBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -75,8 +81,48 @@ public class HomePageActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomePageActivity.this, AddRule.class);
-                startActivity(intent);
+//                Intent intent = new Intent(HomePageActivity.this, AddRule.class);
+//                startActivity(intent);
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(HomePageActivity.this, R.style.BottomSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dialog_support, (LinearLayout)findViewById(R.id.container));
+
+                EditText user_email = bottomSheetView.findViewById(R.id.user_email);
+                user_email.setText(Global.EMAIL);
+
+                EditText question = bottomSheetView.findViewById(R.id.question);
+                Button btn_send = bottomSheetView.findViewById(R.id.send);
+                btn_send.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String user_email_text, question_text;
+                        user_email_text = user_email.getText().toString();
+                        question_text = question.getText().toString();
+
+                        int SDK_INT = android.os.Build.VERSION.SDK_INT; // check
+                        if (SDK_INT > 8) {
+                            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                                    .permitAll().build();
+                            StrictMode.setThreadPolicy(policy);
+                            JSONObject request = new JSONObject();
+                            try {
+                                request.put("Command", "send_question");
+                                request.put("Email", user_email_text);
+                                request.put("Question", question_text);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            output.println(request);
+                            output.flush();
+                            Toast.makeText(HomePageActivity.this, R.string.send_question_ok, Toast.LENGTH_LONG).show();
+                    }
+                    bottomSheetDialog.dismiss();
+                    }
+
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
             }
         });
 
