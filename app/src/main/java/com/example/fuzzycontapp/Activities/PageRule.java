dc98d46a10e4c32c4db3cda328a5798eafafd488
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-06-15 22:00:32 +0300

    creator

--- app/src/main/java/com/example/fuzzycontapp/Activities/PageRule.java
+++ app/src/main/java/com/example/fuzzycontapp/Activities/PageRule.java
@@ -7,10 +7,12 @@ import static com.example.fuzzycontapp.Fragments.HomeFragment.rule_models;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.fuzzycontapp.R;
 import com.example.fuzzycontapp.databinding.ActivityPageRuleBinding;
 
 import java.util.ArrayList;
@@ -19,6 +21,7 @@ public class PageRule extends AppCompatActivity {
     ActivityPageRuleBinding binding;
     ArrayList<Bitmap> all_imgs;
     int position;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +29,20 @@ public class PageRule extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         position = getIntent().getIntExtra("POS", 0);
-        binding.name.setText(getIntent().getStringExtra("NAME"));
+        binding.name.setText(getResources().getString(R.string.by) + " " + getIntent().getStringExtra("NAME"));
         all_imgs = rule_models.get(position).getImgs();
         binding.x.setImageBitmap(all_imgs.get(0));
         binding.v.setImageBitmap(all_imgs.get(1));
         binding.w.setImageBitmap(all_imgs.get(2));
+
+        binding.save.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForColorStateLists")
+            @Override
+            public void onClick(View v) {
+                binding.save.setText(getResources().getString(R.string.saved_data));
+                binding.save.setBackgroundTintList(getResources().getColorStateList(R.color.purple_700));
+            }
+        });
     }
 
 }
