1f9884ee1ea6d68a38ee6684f41e2085167543b2
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-06-15 22:00:12 +0300

    two types of optim

--- app/src/main/java/com/example/fuzzycontapp/Activities/Optimization.java
+++ app/src/main/java/com/example/fuzzycontapp/Activities/Optimization.java
@@ -2,122 +2,115 @@ package com.example.fuzzycontapp.Activities;
 
 import static com.example.fuzzycontapp.Activities.MainActivity.MyThread.input;
 import static com.example.fuzzycontapp.Activities.MainActivity.MyThread.output;
+import static com.example.fuzzycontapp.Activities.MainActivity.MyThread.sock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fuzzycontapp.Adapters.MainAdapter;
 import com.example.fuzzycontapp.Adapters.OptimAdapter;
+import com.example.fuzzycontapp.Fragments.BasinHopping;
+import com.example.fuzzycontapp.Fragments.GeneticAlgorithm;
+import com.example.fuzzycontapp.Fragments.MyRules;
+import com.example.fuzzycontapp.Fragments.SavedRules;
+import com.example.fuzzycontapp.Indiv.Global;
 import com.example.fuzzycontapp.R;
 import com.example.fuzzycontapp.databinding.ActivityOptimizationBinding;
 import com.google.android.material.tabs.TabLayout;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
-public class Optimization extends AppCompatActivity {
+public class Optimization extends AppCompatActivity implements View.OnClickListener{
     ActivityOptimizationBinding binding;
-    OptimAdapter mainAdapter;
-    public static String title, x_start, x_finish, iterations, step;
+    ColorStateList def;
+    TextView select;
+    AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityOptimizationBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        Global.INFO = 0;
+//        item1 = findViewById(R.id.item1);
+//        item2 = findViewById(R.id.item2);
+        binding.include.item1.setOnClickListener(this);
+        binding.include.item2.setOnClickListener(this);
+        select = findViewById(R.id.select);
+        def = binding.include.item2.getTextColors();
+        FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+        fm.replace(R.id.fragment_container, new GeneticAlgorithm());
+        fm.commit();
 
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(R.string.genetic_algorithm));
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText(R.string.basin_hopping));
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Die?");
 
-        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        View view2 = getLayoutInflater().inflate(R.layout.dialog_info, null);
+        builder.setView(view2);
+        dialog = builder.create();
+
+        binding.info.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                binding.viewPager.setCurrentItem(tab.getPosition());
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+            public void onClick(View v) {
+                TextView info = view2.findViewById(R.id.textView4);
+                if (Global.INFO == 0){
+                    info.setText(R.string.info_gen);
+                }
+                else{
+                    info.setText(R.string.info_basin);
+                }
+                dialog.show();
             }
         });
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        mainAdapter = new OptimAdapter(fragmentManager, getLifecycle());
-        binding.viewPager.setAdapter(mainAdapter);
-
-//        binding.optim.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                title = binding.name.getText().toString();
-//                x_start = binding.xStart.getText().toString();
-//                x_finish = binding.xFinish.getText().toString();
-//                iterations = binding.iterations.getText().toString();
-//                step = binding.step.getText().toString();
-//                SendData sendData = new SendData();
-//                sendData.start();
-//                binding.name.setText("");
-//                binding.xStart.setText("");
-//                binding.xFinish.setText("");
-//                binding.iterations.setText("");
-//                binding.step.setText("");
-//                Toast.makeText(Optimization.this, R.string.saved, Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
         binding.back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
+
     }
 
 
-    private static void send_data(){
-        String s = "";
-        int SDK_INT = android.os.Build.VERSION.SDK_INT; // check
-        if (SDK_INT > 8)
-        {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                    .permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            JSONObject request = new JSONObject();
-            try {
-                request.put("Command", "optimize");
-                request.put("Title", title);
-                request.put("x_start", x_start);
-                request.put("x_finish", x_finish);
-                request.put("iterations", iterations);
-                request.put("step", step);
-                request.put("type", "public");
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            output.println(request);
-            output.flush();
-            try {
-                while(!input.ready());
-                s = input.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    static class SendData extends Thread {
-        @Override
-        public void run() {
-            send_data();
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.item1){
+            select.animate().x(0).setDuration(100);
+            binding.include.item1.setTextColor(Color.WHITE);
+            binding.include.item2.setTextColor(def);
+            Global.INFO = 0;
+            FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+            fm.replace(R.id.fragment_container, new GeneticAlgorithm());
+            fm.commit();
+        } else if (v.getId() == R.id.item2){
+            binding.include.item1.setTextColor(def);
+            Global.INFO = 1;
+            binding.include.item2.setTextColor(Color.WHITE);
+            int size = binding.include.item2.getWidth();
+            select.animate().x(size).setDuration(100);
+            FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+            fm.replace(R.id.fragment_container, new BasinHopping());
+            fm.commit();
         }
     }
 }
